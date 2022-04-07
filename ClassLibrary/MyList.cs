c078ff89e4bc835e7e215b0f98ac4d9adb66afe8
unknown <14rvxpg@code.edu.az>
2022-04-07 16:05:59 +0400

    collections

---
+++ ClassLibrary/MyList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary
+{
+    public class MyList<T>
+    {
+        public MyList(T Num)
+        {
+            this.num = Num;
+        }
+
+        public T num;
+        public T no;
+    }
+}
